a693eae6dbb84da53350267383f819f9aa67669e
Jr9phil <jr9phil@gmail.com>
2026-02-08 18:14:41 -0600

    priority icon color fix

--- frontend/swoosh-web/src/components/TaskItem.vue
+++ frontend/swoosh-web/src/components/TaskItem.vue
@@ -42,7 +42,7 @@ setInterval(() => {
 
 const PRIORITIES = [
   { value: 0, icon: ChessPawn, label: 'Default', style: 'btn-ghost opacity-0 group-hover:opacity-50' },
-  { value: 1, icon: ChessKnight, label: 'Medium', style: 'btn-soft text-slate-400' },
+  { value: 1, icon: ChessKnight, label: 'Medium', style: 'btn-soft text-slate-400 bg-slate-400/10' },
   { value: 2, icon: ChessQueen, label: 'High', style: 'btn-soft text-blue-400 bg-blue-400/10' },
   { value: 3, icon: ChessKing, label: 'Top', style: 'btn-soft text-amber-400 bg-amber-400/10' }
 ]
