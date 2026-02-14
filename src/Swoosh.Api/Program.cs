187c55351f554b06b31862d2a0fe2f6e11bcafce
Jr9phil <jr9phil@gmail.com>
2026-02-13 22:16:33 -0600

    containerized the API and database with Docker

--- src/Swoosh.Api/Program.cs
+++ src/Swoosh.Api/Program.cs
@@ -48,6 +48,13 @@ builder.Services.AddCors(options =>
 
 var app = builder.Build();
 
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    db.Database.Migrate();
+}
+
+
 app.UseHttpsRedirection();
 app.UseCors("frontend");
 app.UseAuthentication();
