627a87c74c2c3f0b4a089301853774b4eacd57ea
Jr9phil <jr9phil@gmail.com>
2026-03-30 21:40:17 -0500

    applied date fix to subtasks edit component

--- frontend/swoosh-web/src/components/SubtaskEdit.vue
+++ frontend/swoosh-web/src/components/SubtaskEdit.vue
@@ -58,18 +58,21 @@ const combinedDeadline = computed(() => {
   let date = editedDate.value
   let time = editedTime.value
   if (date && !time) time = '23:59'
-  else if (!date && time) date = new Date().toISOString().split('T')[0]
+  else if (!date && time) date = localDateString(new Date())
   return `${date}T${time}:00`
 })
 
+function localDateString(d: Date) {
+  return `${d.getFullYear()}-${String(d.getMonth() + 1).padStart(2, '0')}-${String(d.getDate()).padStart(2, '0')}`
+}
 function setToday() {
-  editedDate.value = new Date().toISOString().split('T')[0]
+  editedDate.value = localDateString(new Date())
   deadlineError.value = false
 }
 function setTomorrow() {
   const d = new Date()
   d.setDate(d.getDate() + 1)
-  editedDate.value = d.toISOString().split('T')[0]
+  editedDate.value = localDateString(d)
   deadlineError.value = false
 }
 function clearDate() {
